8ea41f632edff9b9d506375d5400b90a42e676da
Dayu <yuguangwei@neigou.com>
2020-07-18 11:39:44 +0800

    重新命名变量

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,9 @@ set -e
 
 qshell account ${QINIU_AK} ${QINIU_SK} dayu
 
+mkdir -p ~/.ssh/
+echo "$SERVER_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+ssh-keyscan $SERVER_ADDR >> ~/.ssh/known_hosts
 
 exec "$@"
