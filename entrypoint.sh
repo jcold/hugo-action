3bae7067e997d3cfd1e0c06388942fc344740a1d
Dayu <yuguangwei@neigou.com>
2020-07-18 12:24:06 +0800

    fix ssh-keyscan port

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ qshell account ${INPUT_AK} ${INPUT_SK} dayu
 mkdir -p ~/.ssh/
 echo "${INPUT_SERVER_KEY}" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-ssh-keyscan ${INPUT_SERVER_ADDR} >> ~/.ssh/known_hosts
+ssh-keyscan -p 11022 ${INPUT_SERVER_ADDR} >> ~/.ssh/known_hosts
 
 exec "$@"
