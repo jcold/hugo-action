a719ee793329ee53adea5023f2ed49bf6c5aadd2
Dayu <yuguangwei@neigou.com>
2020-07-18 12:16:19 +0800

    fix env name

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-qshell account ${AK} ${SK} dayu
+qshell account ${INPUT_AK} ${INPUT_SK} dayu
 
 mkdir -p ~/.ssh/
-echo "$SERVER_KEY" > ~/.ssh/id_rsa
+echo "${INPUT_SERVER_KEY}" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-ssh-keyscan $SERVER_ADDR >> ~/.ssh/known_hosts
+ssh-keyscan ${INPUT_SERVER_ADDR} >> ~/.ssh/known_hosts
 
 exec "$@"
