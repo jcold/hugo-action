f1184f084f28a5c485af24d40aac5f5a5aaad373
Dayu <yuguangwei@neigou.com>
2020-07-18 15:50:44 +0800

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ mkdir -p ~/.ssh/
 echo "${INPUT_SERVER_KEY}" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-keyscan -p 11022 ${INPUT_SERVER_ADDR} >> ~/.ssh/known_hosts
+cat  ~/.ssh/known_hosts
 
 # exec "$@"
 sh -c "$*"
